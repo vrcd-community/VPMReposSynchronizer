596dd85af32ee1319af1083b4c475c29d4436ba8
Misaka-L <lipww1234@foxmail.com>
2024-02-07 00:00:48 +0800

    fix: i forget to add a .zip

--- VPMReposSynchronizer.Core/Services/RepoBrowserService.cs
+++ VPMReposSynchronizer.Core/Services/RepoBrowserService.cs
@@ -100,7 +100,7 @@ public class RepoBrowserService(
     {
         var vpmPackage = mapper.Map<VpmPackage>(package);
 
-        var fileDownloadEndpoint = new Uri(fileHostOptions.Value.BaseUrl, $"files/download/{package.UpstreamId}@{package.PackageId}").ToString();
+        var fileDownloadEndpoint = new Uri(fileHostOptions.Value.BaseUrl, $"files/download/{package.UpstreamId}@{package.PackageId}.zip").ToString();
         vpmPackage.Url = QueryHelpers.AddQueryString(fileDownloadEndpoint, "fileId", package.FileId);
 
         return vpmPackage;
