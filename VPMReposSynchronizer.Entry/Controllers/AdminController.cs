7bed0afcb1440c608dbe8938f9056a013a67585f
Misaka-L <lipww1234@foxmail.com>
2024-04-20 12:11:30 +0800

    chore: ignore appsettings.json

---
+++ VPMReposSynchronizer.Entry/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using VPMReposSynchronizer.Core.Services;
+
+namespace VPMReposSynchronizer.Entry.Controllers;
+
+[ApiController]
+[Route("admin")]
+public class AdminController(RepoSyncTaskService repoSyncTaskService, IMapper mapper) : ControllerBase
+{
+}
